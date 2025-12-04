9bde3a13e1ed68e49b2f57feaad7bbd01a8919fc
Thasmiga <vimalthasmiga1112@gmail.com>
2025-12-04 10:07:39 +0530

    models add

---
+++ models/index.js
@@ -0,0 +1,8 @@
+import "./user.js";
+import "./partnerClothes.js";
+import "./stylerClothes.js";
+import "./occasion.js";
+import "./payment.js";
+import "./styler.js";
+import "./partner.js";
+import "../models/index.js";
